d570c8d80f423e2b38e766ecde4f0a4cfb88ad2a
truong92cdv <truong92cdv@gmail.com>
2025-01-25 00:23:20 +0700

    Add files via upload

---
+++ src/clk_divider.v
@@ -0,0 +1,23 @@
+module clk_divider #(
+	parameter   input_clk_freq  = 100_000_000,      // Input clock 100 MHz
+    parameter   output_clk_freq =   1_000_000       // Output clock  1 MHz = 1 us
+)(
+    input       clk,
+    output      clk_1MHz                                   
+);
+    reg         clk_1MHz_temp = 0;
+    integer     count = 0;
+    integer     half_cycle = input_clk_freq / output_clk_freq / 2 - 1;
+    
+    always @(posedge clk) begin
+        if (count == half_cycle) begin
+            clk_1MHz_temp <= ~clk_1MHz_temp;
+            count <= 0;
+        end else
+            count <= count + 1;
+    end
+
+    assign clk_1MHz = clk_1MHz_temp;
+    
+endmodule
+
---
+++ src/top.v
@@ -0,0 +1,48 @@
+module top(
+    input           clk,
+    input           rst_n,
+    output          scl,
+    inout           sda
+);
+
+    wire            clk_1MHz;
+    wire            done_write;
+    wire [7:0]      data;
+    wire            cmd_data;
+    wire            ena_write;
+    wire [127:0]    row1;
+    wire [127:0]    row2;
+
+    assign row1 =   " Happy New Year ";
+    assign row2 =   "      2025      ";
+
+    clk_divider clk_1MHz_gen(
+        .clk        (clk),
+        .clk_1MHz   (clk_1MHz)
+    );
+
+    lcd_display lcd_display_inst(
+        .clk_1MHz   (clk_1MHz),
+        .rst_n      (rst_n),
+        .ena        (1'b1),
+        .done_write (done_write),
+        .row1       (row1),
+        .row2       (row2),
+        .data       (data),
+        .cmd_data   (cmd_data),
+        .ena_write  (ena_write)
+    );
+
+    lcd_write_cmd_data lcd_write_cmd_data_inst(
+        .clk_1MHz   (clk_1MHz),
+        .rst_n      (rst_n),
+        .data       (data),
+        .cmd_data   (cmd_data),
+        .ena        (ena_write),
+        .i2c_addr   (7'h27),
+        .sda        (sda),
+        .scl        (scl),
+        .done       (done_write)
+    );
+
+endmodule
